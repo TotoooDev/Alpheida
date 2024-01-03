da9866a0ee43ba3bfc4cb25ba3d62ef25e611392
TotoooDev <totooodev@gmail.com>
2024-01-03 14:01:58 +0100

    removed game

--- include/game/controller.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
-
-#include <window.h>
-
-void controller_on_event(void* user_pointer, SDL_Event event);
-
-#endif
-
--- src/main.c
+++ src/main.c
@@ -1,12 +1,7 @@
 #include <app.h>
-#include <game/new_scene.h>
 
 int main(/*int argc, char* argv*/) {
     app_create("Alpheida: Deluxe Edition");
-
-    NewScene* scene = newscene_new();
-    app_set_current_scene(newscene_get_scene(scene));
-
     app_run();
 
     newscene_delete(scene);
