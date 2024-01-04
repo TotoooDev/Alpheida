2cd3f5aa6bcd3b7ad2faac99e4e8d6347111d041
TotoooDev <totooodev@gmail.com>
2024-01-04 20:13:03 +0100

    added wii window init

--- src/platform/wii/wii_window.c
+++ src/platform/wii/wii_window.c
@@ -4,117 +4,117 @@
 
 #include <window.h>
 #include <log.h>
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
-#include <SDL/SDL_mixer.h>
+#include <gccore.h>
+#include <wiiuse/wpad.h>
+#include <ogc/tpl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
 
-#define AUDIO_FREQUENCY 48000
-#define NUM_CHANNELS 2
+#define DEFAULT_FIFO_SIZE	(256*1024)
 
 typedef struct Window {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
+    GXRModeObj* video_settings;
 
-    bool is_open;
+    void* framebuffers[2];
+    unsigned int framebuffer_index;
 } Window;
 
-// global varibales (bad practice i know) (number of fucks given: 0)
-static unsigned int num_windows = 0;
-
-void window_init_sdl() {
-    log_assert(SDL_Init(SDL_INIT_EVERYTHING) == 0, "failed to initialize sdl! sdl error: %s\n", SDL_GetError());
-    log_assert(IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP) != 0, "failed to initialize sdl image! img error: %s\n", IMG_GetError());
-    log_assert(Mix_Init(MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG | MIX_INIT_MID | MIX_INIT_OPUS) != 0, "failed to initialize sdl mixer! mix error: %s", Mix_GetError());
-    log_assert(Mix_OpenAudio(AUDIO_FREQUENCY, AUDIO_FORMAT, AUDIO_NUM_CHANNELS, AUDIO_CHUNK_SIZE) != -1, "failed to open audio device! mix error: %s\n", Mix_GetError());    
-
-    SDL_InitSubSystem(SDL_INIT_JOYSTICK);
-    SDL_JoystickEventState(SDL_ENABLE);
-    SDL_JoystickOpen(0);
-}
-
 Window* window_new(const char* title, int width, int height) {
-    // Initialize SDL if this is the first window
-    if (num_windows <= 0) {
-        window_init_sdl();
-    }
-    
     Window* window = (Window*)malloc(sizeof(Window));
 
-    window->window = SDL_CreateWindow(
-        title,
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        width,
-        height,
-        SDL_WINDOW_SHOWN
-    );
-    log_assert(window->window != NULL, "failed to create window! sdl error: %s\n", SDL_GetError());
+    VIDEO_Init();
 
-    window->renderer = SDL_CreateRenderer(
-        window->window,
-        -1,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-    );
-    log_assert(window->renderer != NULL, "failed to create renderer! sdl error: %s\n", SDL_GetError());
+    window->video_settings = VIDEO_GetPreferredMode(NULL);
 
-    window->is_open = true;
+    // allocate two framebuffers for double buffering
+    window->framebuffers[0] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(window->video_settings));
+    window->framebuffers[1] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(window->video_settings));
+    window->framebuffer_index = 0;
 
-    num_windows++;
+    VIDEO_Configure(window->video_settings);
+    VIDEO_SetNextFramebuffer(window->framebuffers[window->framebuffer_index]);
+    VIDEO_SetBlack(false);
+    VIDEO_Flush();
+    VIDEO_WaitVSync();
+
+    if (window->video_settings->viTVMode & VI_NON_INTERLACE)
+        VIDEO_WaitVSync();
+
+    window->framebuffer_index ^= 1;
+
+    // setup the fifo then init the flipper
+    void* gp_fifo = memalign(32, DEFAULT_FIFO_SIZE);
+    memset(gp_fifo, 0, DEFAULT_FIFO_SIZE);
+
+    GX_Init(gp_fifo, DEFAULT_FIFO_SIZE);
+
+    window_clear(window);
+
+    // gx setup
+    GX_SetViewport(0, 0, window->video_settings->fbWidth, window->video_settings->efbHeight, 0, 1);
+    float y_scale = GX_GetYScaleFactor(window->video_settings->efb_height, window->video_settings->xfbHeight);
+    float xfbHeight = GX_SetDispCopyYScale(y_scale);
+    GX_SetScissor(0, 0, window->video_settings->fbWidth, window->video_settings->efbHeight);
+    GX_SetDispCopySrc(0, 0, window->video_settings->fbWidth, window->video_settings->efbHeight);
+    GX_SetDispCopyDst(window->video_settings->fbWidth, xfbHeight);
+    GX_SetCopyFilter(window->video_settings->aa, window->video_settings->sample_pattern, GX_TRUE, window->video_settings->vfilter);
+    GX_SetFieldMode(window->video_settings->field_rendering, (window->video_settings->viHeight == 2 * window->video_settings->xfbHeight) ? GX_ENABLE : GX_DISABLE);
+
+    if (window->video_settings->aa)
+        GX_SetPixelFmt(GX_PF_RGB565_Z16, GX_ZC_LINEAR);
+    else
+        GX_SetPixelFmt(GX_PF_RGB8_Z24, GX_ZC_LINEAR);
+
+    GX_SetCullMode(GX_CULL_NONE);
+    GX_CopyDisp(window->framebuffers[window->framebuffer_index], GX_TRUE);
+    GX_SetDispCopyGamma(GX_GM_1_0);
+
+    // setup the vertex descriptor
+    // tell the flipper to expect direct data
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XY, GX_F32, 0);
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_POS_ST, GX_F32, 0);
+
+    GX_SetNumChans(1);
+    GX_SetNumTexGens(1);
+    GX_SetTevOp(GX_TEVSTAGE0, GX_REPLACE);
+    GX_SetTevOrder(GX_TEVSTAGE0, GX_TEXCOORD0, GX_TEXMAP0, GX_COLOR0A0);
+    GX_SetTexCoordGen(GX_TEXCOORD0, GX_TG_MTX2x4, GX_TG_TEX0, GX_IDENTITY);
+
+    GX_InvalidateTexAll();
+
+    Mtx44 perspective;
+    guOrtho(perspective, 0, 479, 0, 639, 0, 300);
+    GX_LoadProjectionMtx(perspective, GX_ORTHOGRAPHIC);
+
+    WPAD_Init();
+    
     return window;
 }
 
 void window_delete(Window* window) {
-    SDL_DestroyRenderer(window->renderer);
-    SDL_DestroyWindow(window->window);
     free(window);
-
-    num_windows--;
-    
-    // If there is no window left, quit SDL
-    if (num_windows <= 0) {
-        Mix_CloseAudio();
-        Mix_Quit();
-        IMG_Quit();
-        SDL_Quit();
-    }
 }
 
 void window_clear(Window* window) {
-    SDL_SetRenderDrawColor(window->renderer, 0x7F, 0x7F, 0x7F, 0xFF);
-    SDL_RenderClear(window->renderer);
+    GXColor background = { 127, 127, 127, 255 };
+    GX_SetCopyClear(background, 0x00FFFFFF);
 }
 
 void window_present(Window* window) {
-    SDL_RenderPresent(window->renderer);
+
 }
 
 void window_render_texture(Window* window, Texture* texture, AABB* src, AABB* dest) {
-    SDL_Rect src_rect = { (int)src->x, (int)src->y, (int)src->width, (int)src->height };
-    SDL_FRect dest_rect = { dest->x, dest->y, dest->width, dest->height };
-    SDL_RenderCopyF(window->renderer, texture_get_native_texture(texture), &src_rect, &dest_rect);
+    
 }
 
 void window_render_full_texture(Window* window, Texture* texture, AABB* dest) {
-    SDL_FRect dest_rect = { dest->x, dest->y, dest->width, dest->height };
-    SDL_RenderCopyF(window->renderer, texture_get_native_texture(texture), NULL, &dest_rect);
+    
 }
 
 void window_render_color(Window* window, Color color, AABB* dest) {
-    SDL_SetRenderDrawColor(window->renderer, color.r, color.g, color.b, color.a);
-    SDL_FRect dest_rect = { dest->x, dest->y, dest->width, dest->height };
-    SDL_RenderFillRectF(window->renderer, &dest_rect);
-}
-
-SDL_Window* window_get_native_window(Window* window) {
-    return window->window;
-}
-
-SDL_Renderer* window_get_renderer(Window* window) {
-    return window->renderer;
-}
-
-bool window_is_open(Window* window) {
-    return window->is_open;
+    
 }
 
 #endif
