64025935056fa898a19f29c048206f76b1485e2d
TotoooDev <totooodev@gmail.com>
2024-01-06 18:41:38 +0100

    added wii to fs.h

--- include/platform/fs.h
+++ include/platform/fs.h
@@ -6,10 +6,14 @@
 #ifdef SHRIMP_SWITCH
     #define fs_get_path_romfs(path) "romfs:/"path
     #define fs_get_path(path) "sdmc:/switch/Alpheida/"path
-#else
+#endif
+#ifdef SHRIMP_WII
+    #define fs_get_path_romfs(path) path
+    #define fs_get_path(path) path
+#endif
+#ifdef SHRIMP_LINUX
     #define fs_get_path_romfs(path) path
     #define fs_get_path(path) path
 #endif
 
 #endif
-
