61b7c1abc20da3ad1d42c469e120aa66f8a891ae
TotoooDev <totooodev@gmail.com>
2024-01-14 19:57:10 +0100

    removed unused variable

--- src/platform/wii/wii_texture.c
+++ src/platform/wii/wii_texture.c
@@ -11,8 +11,6 @@
 #include <shrimp.h>
 #include <shrimp_tpl.h>
 
-static int num_textures_created = 0;
-
 typedef struct Texture {
     GXTexObj texture;
 } Texture;
@@ -24,8 +22,6 @@ Texture* texture_new(const char* filename) {
     log_assert(TPL_OpenTPLFromMemory(&file, (void*)shrimp_tpl, shrimp_tpl_size) != -1, "failed to open image %s!", filename);
     log_assert(TPL_GetTexture(&file, 0, &(texture->texture)) != -1, "failed to create texture object for image %s!\n", filename);
     TPL_CloseTPLFile(&file);
-
-    num_textures_created++;
 
     return texture;
 }
