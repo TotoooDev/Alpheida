a6de17a7d6a95a3cca142d0cf8f2c16144395bb4
TotoooDev <totooodev@gmail.com>
2024-01-16 09:37:52 +0100

    added some math

---
+++ include/math/vec2.h
@@ -0,0 +1,19 @@
+#ifndef MATH_VEC2_H
+#define MATH_VEC2_H
+
+#include <types.h>
+
+typedef struct Vec2 {
+    f32 x, y;
+} Vec2;
+
+Vec2 vec2_add(Vec2 a, Vec2 b);
+Vec2 vec2_sub(Vec2 a, Vec2 b);
+Vec2 vec2_mul(Vec2 vec, f32 x);
+Vec2 vec2_div(Vec2 vec, f32 x);
+
+f32 vec_length(Vec2 vec);
+f32 vec2_dot(Vec2 a, Vec2 b);
+Vec2 vec2_normalize(Vec2 vec);
+
+#endif
---
+++ src/math/vec2.c
@@ -0,0 +1,30 @@
+#include <math/vec2.h>
+#include <math.h>
+
+Vec2 vec2_add(Vec2 a, Vec2 b) {
+    return (Vec2){ a.x + b.x, a.y + b.y };
+}
+
+Vec2 vec2_sub(Vec2 a, Vec2 b) {
+    return (Vec2){ a.x - b.x, a.y - b.y };
+}
+
+Vec2 vec2_mul(Vec2 vec, f32 x) {
+    return (Vec2){ vec.x * x, vec.y * x };
+}
+
+Vec2 vec2_div(Vec2 vec, f32 x) {
+    return (Vec2){ vec.x / x, vec.y / x };
+}
+
+f32 vec_length(Vec2 vec) {
+    return sqrtf(vec.x * vec.x + vec.y * vec.y);
+}
+
+f32 vec2_dot(Vec2 a, Vec2 b) {
+    return a.x * b.x + a.y * b.y;
+}
+
+Vec2 vec2_normalize(Vec2 vec) {
+    return vec2_div(vec, vec_length(vec));
+}
