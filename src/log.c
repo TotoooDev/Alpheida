453bd1fe53241e0f8374fb905a514d5638e279e2
TotoooDev <totooodev@gmail.com>
2024-01-16 08:48:24 +0100

    removed log file (useless af)

--- src/log.c
+++ src/log.c
@@ -11,14 +11,6 @@ void log_log(FILE* out, const char* msg, va_list parameters) {
 #ifdef SHRIMP_DEBUG
     // print to stdout
     vfprintf(out, msg, parameters);
-
-    // print to log file
-    FILE* file = fopen(fs_get_path(LOG_FILE_PATH), "a+");
-    // i would love to use log_assert here but log_assert calls log_log so it's not a good idea lol
-    if (file == NULL)
-        fprintf(stderr, "failed to open log file %s! something is terribly wrong...\n", LOG_FILE_PATH);
-    vfprintf(file, msg, parameters);
-    fclose(file);
 #endif
 }
 
