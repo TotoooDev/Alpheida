bde7b96d5e5e782c0260ad41e3c9fc988cbbed6b
TotoooDev <totooodev@proton.me>
2024-01-27 23:51:30 +0100

    added update events function in switch.c

--- src/engine/platform/switch/switch.c
+++ src/engine/platform/switch/switch.c
@@ -14,6 +14,17 @@
 static PadState pad_state;
 static u64 start_time = 0;
 
+void switch_update_events() {
+    padUpdate(&pad_state);
+    event_set_keys_down(padGetButtonsDown(&pad_state));
+    event_set_keys_up(padGetButtonsUp(&pad_state));
+
+    HidAnalogStickState left_stick = padGetStickPos(&pad_state, 0);
+    HidAnalogStickState right_stick = padGetStickPos(&pad_state, 1);
+    event_set_left_joystick_state((f32)left_stick.x / (f32)JOYSTICK_MAX, (f32)left_stick.y / (f32)JOYSTICK_MAX);
+    event_set_right_joystick_state((f32)right_stick.x / (f32)JOYSTICK_MAX, (f32)right_stick.y / (f32)JOYSTICK_MAX);
+}
+
 void platform_init() {
     start_time = platform_get_time();
 
@@ -34,15 +45,7 @@ void platform_init() {
 
 void platform_update() {
     window_update_resolution(app_get_window());
-
-    padUpdate(&pad_state);
-    event_set_keys_down(padGetButtonsDown(&pad_state));
-    event_set_keys_up(padGetButtonsUp(&pad_state));
-
-    HidAnalogStickState left_stick = padGetStickPos(&pad_state, 0);
-    HidAnalogStickState right_stick = padGetStickPos(&pad_state, 1);
-    event_set_left_joystick_state((f32)left_stick.x / (f32)JOYSTICK_MAX, (f32)left_stick.y / (f32)JOYSTICK_MAX);
-    event_set_right_joystick_state((f32)right_stick.x / (f32)JOYSTICK_MAX, (f32)right_stick.y / (f32)JOYSTICK_MAX);
+    switch_update_events();
 }
 
 void platform_exit() {
@@ -56,7 +59,7 @@ void platform_exit() {
 u32 platform_get_time() {
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
-    u64 time = start.tv_sec * 1000000 + start.tv_nsec / 1000;
+    u64 time = start.tv_sec * 1000000 + start.tv_nsec / 1000; // this time is in us
     time /= 1000; // convert to ms
     return (u32)(time - start_time);
 }
