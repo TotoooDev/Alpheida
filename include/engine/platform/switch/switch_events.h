4927844355aa4ac186033172097dfcf4ba7ccd98
Toto <totooodev@proton.me>
2024-02-05 14:51:58 +0100

    added include file

--- include/engine/platform/switch/switch_events.h
+++ include/engine/platform/switch/switch_events.h
@@ -1,6 +1,8 @@
 #ifndef SWITCH_EVENT_H
 #define SWITCH_EVENT_H
 
+#include <engine/types.h>
+
 void event_set_keys_down(u64 keys);
 void event_set_keys_up(u64 keys);
 void event_set_left_joystick_state(f32 x, f32 y);
