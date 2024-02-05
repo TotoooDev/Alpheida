dfadded3b9b8615ed66d6b0e3d96500990c37823
Toto <totooodev@proton.me>
2024-02-05 14:51:50 +0100

    added doc

--- include/engine/platform/fs.h
+++ include/engine/platform/fs.h
@@ -4,7 +4,14 @@
 #include <engine/platform/config.h>
 
 #ifdef SHRIMP_SWITCH
+    /**
+     * Add "romfs:/" to the begginning of the string to access assets contained in romfs.
+    */
     #define fs_get_path_romfs(path) "romfs:/"path
+
+    /**
+     * Add "sdmc:/switch/Alpheida" to the begginning of the string to access assets contained in the SD card.
+    */
     #define fs_get_path(path) "sdmc:/switch/Alpheida/"path
 #endif
 #ifdef SHRIMP_WII
@@ -12,7 +19,14 @@
     #define fs_get_path(path) path
 #endif
 #ifdef SHRIMP_LINUX
+    /**
+     * Does nothing significant on Linux.
+    */
     #define fs_get_path_romfs(path) path
+
+    /**
+     * Does nothing significant on Linux.
+    */
     #define fs_get_path(path) path
 #endif
 
--- include/engine/platform/linux/linux_window.h
+++ include/engine/platform/linux/linux_window.h
@@ -4,6 +4,11 @@
 #include <engine/graphics/window.h>
 #include <SDL2/SDL.h>
 
+/**
+ * Returns the SDL handle of a window.
+ * @param window The window to get the SDL handle from.
+ * @return The `SDL_Window*` window associated with `window`.
+*/
 SDL_Window* linuxwindow_get_native_window(Window* window);
 
 #endif
