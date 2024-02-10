a1a5c622b395578b510b5a788fe5bc3e0aa8ed88
Toto <totooodev@proton.me>
2024-02-10 15:15:56 +0100

    reworked timer

--- src/engine/timer.c
+++ src/engine/timer.c
@@ -1,8 +1,22 @@
 #include <engine/timer.h>
 #include <time.h>
+#include <stdbool.h>
+
+static bool init_timer = true;
+static struct timespec first_time;
 
 f32 timer_get_time_s() {
-    return (f32)clock() / CLOCKS_PER_SEC;
+    if (init_timer) {
+        clock_gettime(CLOCK_REALTIME, &first_time);
+        init_timer = false;
+    }
+
+    struct timespec now;
+    clock_gettime(CLOCK_REALTIME, &now);
+    f32 seconds = (f32)(now.tv_sec - first_time.tv_sec);
+    f32 nano_seconds = (f32)(now.tv_nsec - first_time.tv_nsec);
+    f32 current = seconds + nano_seconds * 1e-9;
+    return current;
 }
 
 f32 timer_get_time_ms() {
